d7621f446c6d9c982b9b3670fc5caa37de8d6b51
jurahf <jurahf@gmail.com>
2020-01-27 19:41:17 +0500

    Вспомогательные файлы

---
+++ Дополнительные файлы/Доп скрипты/Delete ES.sql
@@ -0,0 +1,58 @@
+declare @es_id int = 9;
+
+delete from GoalStackSet
+where Variable_Id in 
+(select id from VariableSet
+where ExpertSystemVariable_Variable_Id < @es_id)
+
+delete from VariableSet
+where ExpertSystemVariable_Variable_Id < @es_id
+
+delete from ConsultationFactSet
+where Fact_Id in
+(select id from FactSet
+where DomainValue_Id in
+(select id from DomainValueSet
+where DomainDomainValue_DomainValue_Id in
+(select id from DomainSet
+where ExpertSystemDomain_Domain_Id < @es_id)))
+
+
+delete from RuleFactSet
+where Rule_Id in
+(select id from RuleSet
+where ExpertSystemRule_Rule_Id < @es_id)
+
+
+delete from SessionSet
+where SessionConsultation_Session_Id in
+(select id from ConsultationSet
+where ExpertSystem_Id < @es_id)
+
+
+delete from ConsultationSet
+where ExpertSystem_Id < @es_id
+
+
+delete from RuleSet
+where ExpertSystemRule_Rule_Id < @es_id
+
+delete from FactSet
+where DomainValue_Id in
+(select id from DomainValueSet
+where DomainDomainValue_DomainValue_Id in
+(select id from DomainSet
+where ExpertSystemDomain_Domain_Id < @es_id))
+
+
+delete from DomainValueSet
+where DomainDomainValue_DomainValue_Id in
+(select id from DomainSet
+where ExpertSystemDomain_Domain_Id < @es_id)
+
+delete from DomainSet
+where ExpertSystemDomain_Domain_Id < @es_id
+
+
+delete from ExpertSystemSet
+where Id <> @es_id
